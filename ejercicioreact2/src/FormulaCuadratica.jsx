b6753ab31bd1736429713ef6ae72611df150a7e5
makdiel <makdiel.tabora@hotmail.com>
2024-03-18 15:26:27 -0600

    ajustes

--- ejercicioreact2/src/FormulaCuadratica.jsx
+++ ejercicioreact2/src/FormulaCuadratica.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect,useState } from 'react';
 
-export const FormulaCuadratica = () => {
-  const [a, setA] = useState('');c
+ const FormulaCuadratica = () => {
+
+  const [a, setA] = useState('');
   const [b, setB] = useState('');
   const [c, setC] = useState('');
   const [result, setResult] = useState('');
@@ -11,37 +12,37 @@ export const FormulaCuadratica = () => {
     if (discriminant > 0) {
       const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
       const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-      setResult(`Root 1: ${root1}, Root 2: ${root2}`);
+      setResult(`x(1): ${root1}, x(2): ${root2}`);
     } else if (discriminant === 0) {
       const root = -b / (2 * a);
-      setResult(`Root: ${root}`);
+      setResult(`X: ${root}`);
     } else {
-      setResult('No real roots');
+      setResult('Valor irreal');
     }
   }
 
   return (
-    <div>cuadratica
-      <h2> Formula Calculator</h2>
+    <div>
+      <h2> Formula Cuadratica</h2>
       <div>
-        <label>
-          Valor 'a':
-          <input type="number" value={a} onChange={(e) => setA(parseFloat(e.target.value))} />
+        <label>          
+          <span class="input-group-text" id="basic-addon1">Ingrese el Valor de (a) </span>
+          <input type="number" className="form-control" placeholder="Ingrese valor de (a)" value={a} onChange={(e) => setA(parseFloat(e.target.value))} />
         </label>
       </div>
       <div>
-        <label>
-          Valor 'b':
-          <input type="number" value={b} onChange={(e) => setB(parseFloat(e.target.value))} />
+        <label>         
+          <span class="input-group-text" id="basic-addon1">Ingrese el Valor de (b) </span>
+          <input type="number" className="form-control" placeholder="Ingrese valor de (b)" value={b} onChange={(e) => setB(parseFloat(e.target.value))} />
         </label>
       </div>
       <div>
-        <label>
-          Valor 'c':
-          <input type="number" value={c} onChange={(e) => setC(parseFloat(e.target.value))} />
+        <label>         
+          <span class="input-group-text" id="basic-addon1">Ingrese el Valor de (c) </span>
+          <input type="number" className="form-control" placeholder="Ingrese valor de (c)" value={c} onChange={(e) => setC(parseFloat(e.target.value))} />
         </label>
       </div>
-      <button onClick={calculateQuadraticFormula}>Calcular</button>
+      <button type="button" className="btn btn-success" onClick={calculateQuadraticFormula}>Calcular</button>
       <div>
         <h3>Result:</h3>
         <p>{result}</p>
---
+++ ejercicioreact2/src/componentes/Formula.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect,useState } from 'react';
+const Formula = () => {
+
+  const [a, setA] = useState('');
+  const [b, setB] = useState('');
+  const [c, setC] = useState('');
+  
+  //const [result, setResult] = useState('');
+
+
+    return (
+       <>
+       <h1>hola</h1>
+       </> 
+    );
+}
+
+export default Formula;
+  
